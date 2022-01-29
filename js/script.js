893296cf20a2c4a88fc3bd248ba3b086cbd6e106
Hugo <warhugo@gmail.com>
2022-01-29 15:51:01 -0300

    Galeria Imagens

--- js/script.js
+++ js/script.js
@@ -39,3 +39,22 @@ function eventosPergunta(pergunta) {
 }
 
 perguntas.forEach(eventosPergunta);
+
+/* Galeria de Bicicletas */
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;  
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if(media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
